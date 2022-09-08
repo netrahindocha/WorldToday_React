ca78b3157dfefa160e00fffbdc5a37122969daf2
netrahindocha <netrahindocha1@gmail.com>
2022-09-08 20:14:56 +0530

    updating WorldToday 1. dark theme updated 2. added previous & next page functionalities

--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    
+
     constructor() {
         super();
         // console.log("constructor");
@@ -13,13 +13,39 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=48753e7eee184f03b80ebfddb963ecf2&page=1";
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=48753e7eee184f03b80ebfddb963ecf2&page=1&pageSize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
-        this.setState({articles: parsedData.articles})
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
     }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=48753e7eee184f03b80ebfddb963ecf2&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=48753e7eee184f03b80ebfddb963ecf2&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
 
     render() {
         return (
@@ -27,11 +53,14 @@ export class News extends Component {
                 <h1>WorldToday - Top Headlines</h1>
                 <div className="row">
                     {this.state.articles.map((elem) => {
-                        return <div className="col-md-4"  key={elem.url}>
-                            <NewsItem title={elem.title?elem.title.slice(0, 45):""} description={elem.description?elem.description.slice(0, 88):""} imageUrl={elem.urlToImage} newsUrl={elem.url} />
+                        return <div className="col-md-4" key={elem.url}>
+                            <NewsItem title={elem.title ? elem.title.slice(0, 45) : ""} description={elem.description ? elem.description.slice(0, 88) : ""} imageUrl={elem.urlToImage} newsUrl={elem.url} />
                         </div>
                     })}
-
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img className="card-img-top" src={!imageUrl?"https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80":imageUrl} />
+                    <img className="card-img-top" alt='' src={!imageUrl?"https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80":imageUrl} />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
